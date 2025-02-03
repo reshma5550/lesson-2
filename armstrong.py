3becfa5205a2b491e9b0589583862e62559d3ab8
reshma5550 <mohantyreshma555@gmail.com>
2025-02-03 21:02:59 +0530

    demo

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+num=int(input("enter a number "))
+sum=0
+temp=num
+while temp>0:
+ digit=temp%10
+ sum+=digit**3
+ temp//=10
+ print ( digit)
+ print (sum)
+ print ( temp)
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number") 
+
+
